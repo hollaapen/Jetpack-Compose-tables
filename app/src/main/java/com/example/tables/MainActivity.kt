1b0a8c3bd38e294c78c9e2a2272b07062a5fc230
Ezra <holla@apensoftwares.co.ke>
2023-09-17 19:11:48 +0300

    Jetpack compose tables

---
+++ app/src/main/java/com/example/tables/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.tables
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tables.ui.theme.TablesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            TableScreen()
+
+        }
+    }
+}
+
+
+@Composable
+fun RowScope.TableCell(
+    text: String,
+    weight: Float
+) {
+    Text(
+        text = text,
+        Modifier
+            .border(1.dp, Color.Black)
+            .weight(weight)
+            .padding(8.dp)
+    )
+}
+
+@Composable
+fun TableScreen() {
+
+    val tableData = (1..100).mapIndexed { index, item ->
+        index to "Item $index"
+    }
+
+    val column1Weight = .3f
+    val column2Weight = .7f
+
+    LazyColumn(Modifier.fillMaxSize().padding(16.dp)) {
+        // Here is the header
+        item {
+            Row(Modifier.background(Color.Gray)) {
+                TableCell(text = "#", weight = column1Weight)
+                TableCell(text = "Item", weight = column2Weight)
+            }
+        }
+
+        items(tableData) {
+            val (id, text) = it
+            Row(Modifier.fillMaxWidth()) {
+                TableCell(text = id.toString(), weight = column1Weight)
+                TableCell(text = text, weight = column2Weight)
+            }
+        }
+    }
+}
